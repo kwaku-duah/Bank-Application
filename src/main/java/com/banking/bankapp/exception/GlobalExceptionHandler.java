8b96024352d19215acb78f8dd314658dd9dcdc56
Sheikhman_Kalderon <duah229@outlook.com>
2025-10-30 21:22:11 +0000

    feature: add global exception handlder

--- src/main/java/com/banking/bankapp/exception/GlobalExceptionHandler.java
+++ src/main/java/com/banking/bankapp/exception/GlobalExceptionHandler.java
@@ -1,8 +1,19 @@
 package com.banking.bankapp.exception;
 
 
+import com.banking.bankapp.payload.GeneralErrorResponseHandler;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<GeneralErrorResponseHandler> errorHandler(Exception ex) {
+        GeneralErrorResponseHandler handler = new GeneralErrorResponseHandler("Something went wrong. Come back at a later time", HttpStatus.INTERNAL_SERVER_ERROR.value());
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(handler);
+
+    }
+
 }
